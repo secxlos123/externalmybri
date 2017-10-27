44e2f132d7c7a307b6f5419d125458e6ee595298
Luthfan H <luthfan.h@smooets.com>
2017-10-27 10:16:19 +0700

    update content galery

--- resources/views/home/_content-galery.blade.php
+++ resources/views/home/_content-galery.blade.php
@@ -9,8 +9,35 @@
 
 <div class="row">
 
-    @foreach ($properties as $value)
-        @include('property._content-list-property')
+    @foreach ($properties as $property)
+        <div class="col-md-4 bottom30">
+            <div class="property_item">
+                <div class="image">
+
+                    <a href="{{ url('rincian-property/'.$value['prop_slug']) }}">
+                        {!! Html::image($property['prop_photo'], $property['prop_name'], [
+                            'class' => 'img-responsive'
+                        ]) !!}
+                    </a>
+
+                    <div class="price clearfix">
+                        <span class="tag pull-right">Rp. {!! number_format($property['prop_price'], 0, ',', '.') !!},-</span>
+                    </div>
+
+                    <span class="tag_t">{{(isset($property['prop_distance'])) ? $value['distance'] : '0'}} Km</span>
+                    <span class="tag_l">{!! $property['prop_developer_name'] !!}</span>
+
+                </div>
+                <div class="proerty_content">
+                    <div class="proerty_text">
+                        <h3 class="captlize">
+                            <a href="javascript:void(0)">{!! $property['prop_name'] !!}</a>
+                        </h3>
+                        <p>{!! $property['prop_city_name'] !!}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     @endforeach
 
 </div>
