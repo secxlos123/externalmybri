3b7731b5d2ffb479ca39e371a920cca48ac2ed76
Luthfan H <luthfan.h@smooets.com>
2017-10-27 16:18:45 +0700

    update list type prop

--- resources/views/property/_content-type.blade.php
+++ resources/views/property/_content-type.blade.php
@@ -1,8 +1,12 @@
 <div class="col-md-4 col-sm-6 col-xs-12 heading_space">
     <div class="sim-lar-p">
-        <div class="image bottom20"><img src="{{(!isset($types['photos'][0])) ? image_checker() : image_checker($types['photos'][0])}}" alt="image"></div>
+        <a class="btn-more" href="{{ url('rincian-property-type/'.$types['slug']) }}">
+            <div class="image bottom20">
+                <img src="{{(!isset($types['photos'][0])) ? image_checker() : image_checker($types['photos'][0])}}" alt="image">
+            </div>
+        </a>
         <div class="sim-lar-text">
-            <h3 class="bottom10"><a href="#.">{{$types['name']}}</a></h3>
+            <h3 class="bottom10"><a href="{{ url('rincian-property-type/'.$types['slug']) }}">{{$types['name']}}</a></h3>
             <p><span>Tersedia:</span>{{ $types['items']}} Unit <span>|</span> <span>Harga:</span> Rp {{number_format($types['price'], 2)}}</p>
             <p class="bottom20"></p>
             <a class="btn-more" href="{{ url('rincian-property-type/'.$types['slug']) }}">
