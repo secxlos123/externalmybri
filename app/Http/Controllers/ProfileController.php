975116b4a94c6ae7fe2216be3c5fb32606583f94
Luthfan H <luthfan.h@smooets.com>
2017-12-05 15:19:20 +0700

    update to fix redirect profile role nasabah

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -159,7 +159,8 @@ class ProfileController extends Controller
             ->get();
         return view('profile.index_password', [
             'results' => $results['contents'],
-            'type' => 'view'
+            'type' => 'view',
+            'active' => 'password'
         ]);
     }
 
