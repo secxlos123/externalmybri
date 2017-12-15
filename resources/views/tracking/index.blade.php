3e9c86a79323e88a0e5fde0b62239a7f0bbc13fc
Luthfan H <luthfan.h@smooets.com>
2017-12-15 13:21:41 +0700

    update to remove ao name on list tracking

--- resources/views/tracking/index.blade.php
+++ resources/views/tracking/index.blade.php
@@ -76,7 +76,6 @@
                 { data: 'nama_pemohon', name: 'nama_pemohon' },
                 { data: 'developer_name', name: 'developer_name' },
                 { data: 'property_name', name: 'property_name' },
-                { data: 'ao_name', name: 'ao_name' },
                 { data: 'status', name: 'status' },
                 { data: 'action', name: 'action', bSortable: false },
             ],
