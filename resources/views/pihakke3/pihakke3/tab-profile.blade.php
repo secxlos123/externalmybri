3dbb2e0ce0465999f6061d27b86c6519b89d6bf9
Erwan Akse <erwanakse@gmail.com>
2017-12-21 22:33:40 +0700

    change field name phone on pihak-3

--- resources/views/pihakke3/pihakke3/tab-profile.blade.php
+++ resources/views/pihakke3/pihakke3/tab-profile.blade.php
@@ -35,7 +35,7 @@
         <div class="single-query form-group bottom20 {{ $errors->has('phone_number') ? ' has-error' : '' }} ">
             <label>Nomor Telepon (*)</label>
             @if($type != 'view')
-            <input type="text" class="keyword-input numeric" name="phone_number" maxlength="12" minlength="9" value="{{$results['phone_number']}}">
+            <input type="text" class="keyword-input numeric" name="phone" maxlength="12" minlength="9" value="{{$results['phone_number']}}">
             @else
             <p>{{ $results['phone_number'] }}</p>
             @endif
