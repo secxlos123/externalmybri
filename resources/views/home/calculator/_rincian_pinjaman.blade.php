9e29b93b929707099e894ea0fc420e03bd916833
Erwan Akse <erwanakse@gmail.com>
2018-01-31 14:06:47 +0700

    fix calculator

--- resources/views/home/calculator/_rincian_pinjaman.blade.php
+++ resources/views/home/calculator/_rincian_pinjaman.blade.php
@@ -5,7 +5,13 @@
            <div class="form-group">
                 <label class="col-md-4 control-label">Uang Muka </label>
                 <div class="col-md-8">
-                    <label class="col-md-8 control-label">: Rp. <span class="currency"> {{$rincian_pinjaman['rincian']['uang_muka']}}</span></label>
+                    <label class="col-md-8 control-label">: Rp. <span class="currency-rp"> {{$rincian_pinjaman['rincian']['uang_muka']}}</span></label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-4 control-label">Plafond Yang Diajukan </label>
+                <div class="col-md-8">
+                    <label class="col-md-8 control-label">: Rp. <span class="currency-rp"> {{$rincian_pinjaman['rincian']['plafond']}}</span></label>
                 </div>
             </div>
             <?php
@@ -19,14 +25,14 @@
             <div class="form-group">
                 <label class="col-md-4 control-label"> {{$suku_bunga}} </label>
                 <div class="col-md-8">
-                    <label class="col-md-8 control-label">: {{str_replace('.', ',',$rincian_pinjaman['rincian']['suku_bunga'])}} </label>
+                    <label class="col-md-8 control-label">: {{str_replace('.', ',', $rincian_pinjaman['rincian']['suku_bunga'])}} </label>
                 </div>
             </div>       
         @if($interest_rate_type==4 || $interest_rate_type== 3)
             <div class="form-group">
                 <label class="col-md-4 control-label"> Suku Bunga Floating</label>
                 <div class="col-md-8">
-                   <label class="col-md-8 control-label">: {{str_replace('.', ',',$rincian_pinjaman['rincian']['suku_bunga_floating'])}} </label>
+                   <label class="col-md-8 control-label">: {{str_replace('.', ',', $rincian_pinjaman['rincian']['suku_bunga_floating'])}} </label>
                 </div>
             </div>
          @endif   
@@ -55,25 +61,6 @@
                     <label class="col-md-8 control-label">: {{$rincian_pinjaman['rincian']['kredit_fix']}} </label>
                 </div>
             </div>
-            <div class="form-group">
-                <label class="col-md-4 control-label">Angsuran Fixed </label>
-                <div class="col-md-8">
-                   <label class="col-md-8 control-label">: Rp <span class="currency">{{$rincian_pinjaman['rincian']['angsuranFixed']}} </span></label>
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-md-4 control-label">Angsuran Float </label>
-                <div class="col-md-8">
-                   <label class="col-md-8 control-label">: Rp <span class="currency">{{$rincian_pinjaman['rincian']['angsuranFloat']}} </span></label>
-                </div>
-            </div>
-        @else
-            <div class="form-group">
-                <label class="col-md-4 control-label">Angsuran Per Bulan </label>
-                <div class="col-md-8">
-                   <label class="col-md-8 control-label">: Rp <span class="currency">{{$rincian_pinjaman['angsuran_perbulan']}} </span></label>
-                </div>
-            </div>
         @endif    
         @if($interest_rate_type==4)
              <div class="form-group">
@@ -83,10 +70,32 @@
                 </div>
             </div>
         @endif
+        @if($interest_rate_type==3)
+            <div class="form-group">
+                <label class="col-md-4 control-label">Angsuran Fixed </label>
+                <div class="col-md-8">
+                   <label class="col-md-8 control-label">: Rp <span class="currency-rp">{{ $rincian_pinjaman['rincian']['angsuranFixed'] }} </span></label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-4 control-label">Angsuran Float </label>
+                <div class="col-md-8">
+                   <label class="col-md-8 control-label">: Rp <span class="currency-rp">{{ $rincian_pinjaman['rincian']['angsuranFloat'] }} </span></label>
+                </div>
+            </div>
+        @endif
+        @if($interest_rate_type!=3)
+            <div class="form-group">
+                <label class="col-md-4 control-label">Angsuran Per Bulan </label>
+                <div class="col-md-8">
+                   <label class="col-md-8 control-label">: Rp <span class="currency-rp">{{$rincian_pinjaman['angsuran_perbulan']}} </span></label>
+                </div>
+            </div>
+        @endif
             <div class="form-group">
                 <label class="col-md-4 control-label">Pembayaran Pertama</label>
                 <div class="col-md-8">
-                    <label class="col-md-8 control-label">: Rp <span class="currency">{{$rincian_pinjaman['pembayaran_pertama']}}</label>
+                    <label class="col-md-8 control-label">: Rp <span class="currency-rp">{{$rincian_pinjaman['pembayaran_pertama']}}</label>
                 </div>
             </div>  
         </div>
