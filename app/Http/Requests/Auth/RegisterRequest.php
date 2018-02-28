40c132186e2d83f828ff910bcc78a17542708ca0
Rangga Darmajati <rangga.android69@gmail.com>
2018-02-28 17:01:05 +0700

    create recaptcha for stagging

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -60,7 +60,8 @@ class RegisterRequest extends FormRequest
             'email' => 'required|email',
             'fullname' => 'required|string|min:4',
             'phone' => 'nullable|string|regex:/^[0-9]+$/|min:9|max:12',
-            'password' => 'required|min:6|regex:/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/|confirmed'
+            'password' => 'required|min:6|regex:/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/|confirmed',
+            'g-recaptcha-response' => 'required|recaptcha'
         ];
     }
 
