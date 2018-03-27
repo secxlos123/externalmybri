f5f1e08fdc119c5fb608681b6e341d61e6b60802
Rangga Darmajati <rangga.android69@gmail.com>
2018-03-27 11:34:19 +0700

    create description title for mandatory field on developer tab-change-password

--- resources/views/profile/tab-content-change-password.blade.php
+++ resources/views/profile/tab-content-change-password.blade.php
@@ -5,18 +5,18 @@
 @endif
     <div class="col-md-12">
         <div class="single-query form-group bottom20">
-            <label>Password Lama (*)</label>
-            {!! Form::password('old_password', [ 'class' => 'keyword-input', 'placeholder' => 'Kata Sandi lama' ]) !!}
+            <label>Password Lama (<span title="Kolom Password Lama wajib diisi!">*</span>)</label>
+            {!! Form::password('old_password', [ 'class' => 'keyword-input', 'placeholder' => 'Kata Sandi lama', 'title' => 'Kolom Password Lama Wajib diisi!' ]) !!}
         </div>
 
         <div class="single-query form-group bottom20">
-            <label>Password Baru (*)</label>
-            {!! Form::password('password', [ 'class' => 'keyword-input', 'placeholder' => 'Kata Sandi' ]) !!}
+            <label>Password Baru (<span title="Kolom Password Baru wajib diisi!">*</span>)</label>
+            {!! Form::password('password', [ 'class' => 'keyword-input', 'placeholder' => 'Kata Sandi','title' => 'Kolom Password Baru Wajib diisi!' ]) !!}
         </div>
 
         <div class="single-query form-group bottom20">
-            <label>Konfirmasi Password Baru (*)</label>
-            {!! Form::password('password_confirmation', [ 'class' => 'keyword-input', 'placeholder' => 'Ulangi Kata Sandi' ]) !!}
+            <label>Konfirmasi Password Baru (<span title="Kolom Konfirmasi Password Baru Wajib diisi!">*</span>)</label>
+            {!! Form::password('password_confirmation', [ 'class' => 'keyword-input', 'placeholder' => 'Ulangi Kata Sandi', 'title' => 'Kolom Konfirmasi Password Baru Wajib diisi!' ]) !!}
         </div>
     </div>
     @include('form._input_long_lat')
