edcac7da6d869ca1d4b49b3ac1d56927efe48bb1
rangga.darmajati <rangga.darmajati@wgs.co.id>
2018-04-17 14:49:02 +0700

    logic for ssl web or non ssl web

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
+        if( env('IS_SSL', 1) ){
+            \URL::forceScheme('https');
+        }
     }
 
     /**
