0d0edd9a50466be7824764ebd25790f20d3a4f11
root <rangga.darmajati@wgs.co.id>
2018-04-26 01:36:21 +0700

    change service provider to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,17 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Request $request)
     {
-        // if( env('IS_SSL', 1) ){
-        //     \URL::forceScheme('https');
-        // }
-
-        $host = $request->getSchemeAndHttpHost();
-        $cek = substr($host, 0,8);
-        \Log::info($cek);
-        if( $cek != 'http://' ){
-            \Log::info("HTTPS");
-            \URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
     }
 
     /**
